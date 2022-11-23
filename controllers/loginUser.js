6f57d2b7cabafdeffca8f395b519a67e8357826c
Bhupesh Shrestha <bhupesh.stha@gmail.com>
2022-11-23 10:10:13 -0500

    logs

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,19 +1,25 @@
-const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
 module.exports = async (req, res) => {
   const { username, password } = req.body;
-  console.log(req.body)
+  console.log(req.body);
   const user = await User.findOne({ username });
   if (user) {
     const isSame = bcrypt.compare(password, user.password);
+    console.log('isSame ', isSame);
+
     if (isSame) {
       req.session.userId = user._id;
-      res.redirect("/");
+      console.log('isSame ', req.session.userId);
+      console.log('isSame ', user._id);
+      console.log('user ', user);
+
+      res.redirect('/');
     } else {
-      res.redirect("/auth/login");
+      res.redirect('/auth/login');
     }
   } else {
-    res.redirect("/auth/login");
+    res.redirect('/auth/login');
   }
 };
